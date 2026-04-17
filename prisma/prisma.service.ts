cc78da6d1afe3f548b60342633990c9ac03b8ccf
Aliaksei-Varabyou <Aliaksei_Varabyou@epam.com>
2026-04-17 08:23:42 +0200

    fix path

--- prisma/prisma.service.ts
+++ prisma/prisma.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from 'generated/prisma/client';
-import { PrismaPg } from 'node_modules/@prisma/adapter-pg/dist';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
 export class PrismaService
