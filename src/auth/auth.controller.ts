401d05630d11369fa3867c349503a01d338395f4
Aliaksei-Varabyou <Aliaksei_Varabyou@epam.com>
2026-04-19 17:00:39 +0200

    feat(auth): add JWT auth guard and protect routes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,11 +3,13 @@ import { SignupDto } from './dto/signup.dto';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { RefreshDto } from './dto/refresh.dto';
+import { Public } from './decorators/public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @Public()
   @Post('signup')
   async signup(@Body() dto: SignupDto) {
     await this.authService.signUp(dto);
@@ -17,12 +19,14 @@ export class AuthController {
     };
   }
 
+  @Public()
   @Post('login')
   @HttpCode(200)
   async login(@Body() dto: LoginDto) {
     await this.authService.login(dto);
   }
 
+  @Public()
   @Post('refresh')
   @HttpCode(200)
   async refresh(@Body() dto: RefreshDto) {
---
+++ src/auth/jwt/jwt.guard.ts
@@ -0,0 +1,49 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Reflector } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class JwtGuard implements CanActivate {
+  constructor(
+    private jwtService: JwtService,
+    private config: ConfigService,
+    private reflector: Reflector,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>('isPublic', [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) return true;
+
+    const request = context.switchToHttp().getRequest();
+    const authHeader = request.headers('Authorization');
+    if (!authHeader) {
+      throw new UnauthorizedException('No authorization header');
+    }
+
+    const [type, token] = authHeader.split(' ');
+    if (type !== 'Bearer' || !token) {
+      throw new UnauthorizedException('Invalid authorization format');
+    }
+
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: this.config.get('JWT_SECRET_KEY'),
+      });
+      request.user = payload;
+    } catch (e) {
+      throw new UnauthorizedException('Invalid or expured token');
+    }
+
+    return true;
+  }
+}
