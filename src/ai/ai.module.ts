972491aeec1f18360638ec65bcf4aa6910933ecb
Aliaksei-Varabyou <Aliaksei_Varabyou@epam.com>
2026-05-03 09:02:21 +0200

    feat: gemini integration

--- src/ai/ai.module.ts
+++ src/ai/ai.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { AiService } from './ai.service';
 import { AiController } from './ai.controller';
 import { GeminiService } from './gemini/gemini.service';
+import { HttpService } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpService],
   controllers: [AiController],
   providers: [AiService, GeminiService],
   exports: [AiService],
--- src/ai/gemini/gemini.service.ts
+++ src/ai/gemini/gemini.service.ts
@@ -1,4 +1,72 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { ConfigService } from '@nestjs/config';
+import { firstValueFrom } from 'rxjs';
+
 
 @Injectable()
-export class GeminiService {}
+export class GeminiService {
+  private readonly apiKey: string;
+  private readonly baseUrl: string;
+  private readonly model: string;
+  constructor(
+    private readonly httpService: HttpService,
+    private readonly configService: ConfigService,
+  ) {
+    this.apiKey = this.configService.get<string>('GEMINI_API_KEY');
+    this.baseUrl = this.configService.get<string>('GEMINI_API_BASE_URL');
+    this.model = this.configService.get<string>('GEMINI_MODEL');
+  }
+
+  async generateContext(prompt: string): Promise<string> {
+    const url = `${this.baseUrl}/v1beta/models/${this.model}:generateContent?key=${this.apiKey}`;
+
+    try {
+      const response = await firstValueFrom(
+        this.httpService.post(
+          url,
+          {
+            contents: {
+              parts: [{ text: prompt }],
+            },
+          },
+          { timeout: 5000 },
+        ),
+      );
+      const text = response.data?.candidates?.[0]?.content?.parts?.[0]?.text;
+      if (!text) {
+        throw new HttpException(
+          'Invalid response from AI',
+          HttpStatus.BAD_GATEWAY,
+        );
+      }
+      return text;
+    } catch (error: any) {
+      if (error.code === 'ECONNABORTED') {
+        throw new HttpException(
+          'AI service timeout',
+          HttpStatus.SERVICE_UNAVAILABLE,
+        );
+      }
+
+      const status = error.response?.status;
+      if (status === 401 || status === 403) {
+        throw new HttpException(
+          'AI authentificate failed',
+          HttpStatus.INTERNAL_SERVER_ERROR,
+        )
+      }
+      if (status === 429) {
+        throw new HttpException(
+          'AI rate limit exceeded',
+          HttpStatus.SERVICE_UNAVAILABLE,
+        );
+      }
+
+      throw new HttpException(
+        'AI service unavailable',
+        HttpStatus.SERVICE_UNAVAILABLE,
+      );
+    }
+  }
+}
