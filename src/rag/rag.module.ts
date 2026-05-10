946fe988ece1476d95ffe895fc84455b0b58d7ec
Aliaksei-Varabyou <Aliaksei_Varabyou@epam.com>
2026-05-10 15:49:13 +0200

    feat: implement chunking system

--- src/rag/rag.module.ts
+++ src/rag/rag.module.ts
@@ -5,10 +5,11 @@ import { RagService } from './rag.service';
 import { QdrantService } from './qdrant.service';
 import { RagController } from './rag.controller';
 import { AiModule } from 'src/ai/ai.module';
+import { ChunkingService } from './chunking.service';
 
 @Module({
   imports: [ConfigModule, AiModule],
-  providers: [RagService, QdrantService],
+  providers: [RagService, QdrantService, ChunkingService],
   exports: [RagService, QdrantService],
   controllers: [RagController],
 })
